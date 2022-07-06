81938998cb2594ec74374d7ca26be3351d654af8
wodrobina <wodrobina@gmail.com>
2022-07-06 10:54:00 +0200

    Added tests

--- src/test/java/eu/wodrobina/homeenvironment/sensor/InMemoryTestSensorRepository.java
+++ src/test/java/eu/wodrobina/homeenvironment/sensor/InMemoryTestSensorRepository.java
@@ -1,18 +1,10 @@
 package eu.wodrobina.homeenvironment.sensor;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.FluentQuery;
 
-public class InMemoryTestSensorRepository  implements SensorRepository{
+class InMemoryTestSensorRepository implements SensorRepository {
 
     private Map<String, Sensor> values = new ConcurrentHashMap<>();
 
